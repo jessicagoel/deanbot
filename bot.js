dcc1ff479f6bd16e5e857103764b5237e54a2d59
jessicagoel <78863710+jessicagoel@users.noreply.github.com>
2021-09-04 21:41:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ client.on('message', (receivedMessage) => {
         receivedMessage.channel.send(scalevar)
     }
   
-    else if (receivedMessage.content.toLowerCase().includes('cas lobotomy')) {
+    else if (receivedMessage.content.toLowerCase().includes(':eye::drop_of_blood::lips::drop_of_blood::eye:')) {
           const attachment = new Discord.Attachment("https://i.imgur.com/RwhLGmv.jpg")   
           receivedMessage.channel.send(attachment)
     }
